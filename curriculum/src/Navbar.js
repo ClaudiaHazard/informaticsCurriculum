73e4de96fbf051154c8192bd2fda63f1319b6415
Claudia Hazard <claudia.hazard.14@sansano.usm.cl>
2023-10-20 17:49:01 +0200

    Fix routes

--- curriculum/src/Navbar.js
+++ curriculum/src/Navbar.js
@@ -2,9 +2,7 @@ import { Link } from "react-router-dom"
 const Navbar =()=>{
       return (
             <div>
-                  <Link to="/">Home</Link>
-                  <Link to="/about">About</Link>
-                  <Link to="/careers">Careers</Link>
+                  <Link to="/">StudyPlan</Link>
             </div>
       )
 }
---
+++ curriculum/src/routes/StudyPlan.js
@@ -0,0 +1,13 @@
+
+
+function StudyPlan() {
+  return (
+    <>
+    <h2>Title</h2>
+    <button type="button"
+    onclick="document.getElementById('demo').innerHTML = Date()">
+    Click me to display Date and Time.</button>
+    </>
+  );
+}
+export default StudyPlan;
